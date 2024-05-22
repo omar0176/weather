ee88acff7d43c7f854b3553ee96a99c621e68ca1
Turbo <69730868+omar0176@users.noreply.github.com>
2024-05-22 15:45:44 +0200

    Update window.dart
    I ADDED THE NEXT HOUR WEATHER SHIT

--- lib/window.dart
+++ lib/window.dart
@@ -16,7 +16,7 @@ class _MainWindowState extends State<Main_window> {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.lightBlue[100], // Set background color here
+          backgroundColor: const Color(0xFFDCE3EA), // Set background color here
           bottomNavigationBar: NavigationBar(
             onDestinationSelected: (int index) {
               setState(() {
@@ -61,7 +61,7 @@ class _MainWindowState extends State<Main_window> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const SizedBox(height: 50),
+            const SizedBox(height: 20),
             const Center(
               child: Text(
                 'My Location',
@@ -86,8 +86,8 @@ class _MainWindowState extends State<Main_window> {
                   decoration: BoxDecoration(
                     boxShadow: [
                       BoxShadow(
-                        color: Color(0xFFB2E4FA).withOpacity(0.15), 
-                        offset: Offset(0, 4), 
+                        color: Color(0xFFB2E4FA).withOpacity(0.15),
+                        offset: Offset(0, 4),
                         blurRadius: 4,
                         spreadRadius: 0,
                       ),
@@ -97,10 +97,10 @@ class _MainWindowState extends State<Main_window> {
                   ),
                   padding: const EdgeInsets.all(16),
                   margin: const EdgeInsets.all(16),
-                  child: Column(
+                  child: const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
+                    children: [
                       Text('16°', style: TextStyle(fontSize: 80)),
                       Text('<weather_conditions>'),
                       Text('H:<today_high>    L:<today_low>'),
@@ -109,6 +109,130 @@ class _MainWindowState extends State<Main_window> {
                 ),
               ],
             ),
+            Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFB2E4FA).withOpacity(0.15),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                  color: Colors.white.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const Center(
+                          child: Text('1pm'),
+                        ),
+                        Image.asset('images/cloud.png'),
+                        const Center(
+                          child: Text(
+                            '17°',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 28),
+                    Column(
+                      children: [
+                        const Center(
+                          child: Text('2pm'),
+                        ),
+                        Image.asset('images/cloud.png'),
+                        const Center(
+                          child: Text(
+                            '15°',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 28),
+                    Column(
+                      children: [
+                        const Center(
+                          child: Text('3pm'),
+                        ),
+                        Image.asset('images/cloud.png'),
+                        const Center(
+                          child: Text(
+                            '13°',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 28),
+                    Column(
+                      children: [
+                        const Center(
+                          child: Text('4pm'),
+                        ),
+                        Image.asset('images/cloud.png'),
+                        const Center(
+                          child: Text(
+                            '12°',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 28),
+                    Column(
+                      children: [
+                        const Center(
+                          child: Text('5pm'),
+                        ),
+                        Image.asset('images/cloud.png'),
+                        const Center(
+                          child: Text(
+                            '11°',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 28),
+                    Column(
+                      children: [
+                        const Center(
+                          child: Text('6pm'),
+                        ),
+                        Image.asset('images/cloud.png'),
+                        const Center(
+                          child: Text(
+                            '20°',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),),
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/Trees.png'), // Path to your image asset
+              fit: BoxFit.cover, // Adjust this according to your needs
+            ),
+          ),
+        ),
           ],
         ),
       ),
@@ -125,5 +249,8 @@ class _MainWindowState extends State<Main_window> {
         ),
       ),
     );
+  }
+}
+
   }
 }
