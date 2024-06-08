24294f3386aed14e743ea1b094e66425342576c3
Sebastián Zelaya <sebaszelaya1004@gmail.com>
2024-06-08 11:38:07 +0200

    Cleaned up the code

--- lib/bookmarks_page.dart
+++ lib/bookmarks_page.dart
@@ -3,64 +3,140 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'dart:ui';
 import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:geocoding/geocoding.dart';
 
 class BookmarksPage extends StatefulWidget {
-  const BookmarksPage({Key? key}) : super(key: key);
+  const BookmarksPage({super.key});
 
   @override
-  _BookmarksPageState createState() => _BookmarksPageState();
+  BookmarksPageState createState() => BookmarksPageState();
 }
 
 class Bookmark {
   final String location;
+  final String country;
   final String temperature;
   final String highTemp;
   final String lowTemp;
 
   Bookmark({
     required this.location,
+    required this.country,
     required this.temperature,
     required this.highTemp,
     required this.lowTemp,
   });
 }
 
-class _BookmarksPageState extends State<BookmarksPage> {
+class BookmarksPageState extends State<BookmarksPage> {
+  final String _apiKeyOpenweather = 'ca7125e0df61234bbfddef29c1ababde';
+
   final List<Bookmark> _bookmarks = [];
 
   bool _isDialogVisible = false; // Variable to toggle the dialog box
-  String _weatherDescription = "Loading weather...";
   String _temperature = "--";
   String _location = "";
+  String _country = "";
   String _highTemp = "--";
   String _lowTemp = "--";
-  TextEditingController _searchController = TextEditingController();
-  String upperImage = 'images/Sunny/SUN.png';
-  String lowerImage = 'images/Sunny/Trees.png';
+  final TextEditingController _searchController = TextEditingController();
 
-  List<Map<String, dynamic>> _hourlyForecast = [];
-
-  final String _apiKeyOpenweather = 'ca7125e0df61234bbfddef29c1ababde';
-
+  // Initializes state and fetches current location + loads bookmarks
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
     _loadBookmarks();
   }
 
+  // Method to toggle the text box for adding a bookmark
+  void _toggleTextBox() {
+    setState(() {
+      _isDialogVisible = !_isDialogVisible;
+    });
+  }
+
+  // Method to fetch weather data from OpenWeather API given appropriate coordinates
+  // Modifies bookmark with the fetched data
+  Future<void> _fetchWeather(double latitude, double longitude) async {
+    final response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$_apiKeyOpenweather'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        _location = data['name'];
+        _country = data['sys']['country'];
+        _temperature =
+            double.parse(data['main']['temp'].toString()).toStringAsFixed(0);
+        _highTemp = double.parse(data['main']['temp_max'].toString())
+            .toStringAsFixed(0);
+        _lowTemp = double.parse(data['main']['temp_min'].toString())
+            .toStringAsFixed(0);
+        _modifyBookmark(_location, _country, _temperature, _highTemp,
+            _lowTemp); // Call _modifyBookmark here
+      });
+    } else {
+      setState(() {
+        _location = "Failed to fetch.";
+      });
+    }
+  }
+
+// Method to fetch weather data for an inputted location
+  Future<void> _fetchWeatherForSearch(String location) async {
+    try {
+      List<Location> locations = await locationFromAddress(location); // Get the location from the inputted location
+      if (locations.isNotEmpty) { // If the location exists
+        double latitude = locations[0].latitude;
+        double longitude = locations[0].longitude;
+
+        _fetchWeather(latitude, longitude);
+
+        setState(() {
+          _location = location;
+        });
+      }
+    } catch (e) { // If the location doesn't exist
+      setState(() {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Location doesn't exist"),
+                content: const Text("Please enter a valid location."),
+                actions: [
+                  Center(
+                    child: SizedBox(
+                      width: 100,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Center(child: Text('OK')),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+        );
+      });
+    }
+  }
+
+// Load bookmarks from SharedPreferences
   Future<void> _loadBookmarks() async {
     final prefs = await SharedPreferences.getInstance();
     final String? encodedBookmarks = prefs.getString('bookmarks');
     if (encodedBookmarks != null) {
       final List<dynamic> decodedBookmarks = json.decode(encodedBookmarks);
       setState(() {
+        // Add decoded bookmarks to the _bookmarks list
         _bookmarks.addAll(decodedBookmarks.map((bookmark) {
           return Bookmark(
             location: bookmark['location'],
+            country: bookmark['country'],
             temperature: bookmark['temperature'],
             highTemp: bookmark['highTemp'],
             lowTemp: bookmark['lowTemp'],
@@ -70,32 +146,20 @@ class _BookmarksPageState extends State<BookmarksPage> {
     }
   }
 
-  Future<void> _saveBookmarks() async {
-    final prefs = await SharedPreferences.getInstance();
-    final List<Map<String, String>> bookmarks = _bookmarks.map((bookmark) {
-      return {
-        'location': bookmark.location,
-        'temperature': bookmark.temperature,
-        'highTemp': bookmark.highTemp,
-        'lowTemp': bookmark.lowTemp,
-      };
-    }).toList();
-    final encodedBookmarks = json.encode(bookmarks);
-    prefs.setString('bookmarks', encodedBookmarks);
-  }
-
-  void _modifyBookmark(
-      String location, String temperature, String highTemp, String lowTemp) {
+// Modify a bookmark or add a new one
+  void _modifyBookmark(String location, String country, String temperature, String highTemp, String lowTemp) {
     setState(() {
+      // Check if the _bookmarks list has fewer than 20 bookmarks
       if (_bookmarks.length < 20) {
+        // Check if a bookmark with the same location already exists
         if (_bookmarks.any((bookmark) => bookmark.location == location)) {
+          // Show a dialog for a duplicate bookmark
           showDialog(
             context: context,
             builder: (BuildContext context) {
               return AlertDialog(
                 title: const Text('Duplicate Bookmark'),
-                content:
-                    const Text('A bookmark with this location already exists.'),
+                content: const Text('A bookmark with this location already exists.'),
                 actions: <Widget>[
                   Center(
                     child: SizedBox(
@@ -114,13 +178,16 @@ class _BookmarksPageState extends State<BookmarksPage> {
           );
           return;
         } else {
+          // Create a new bookmark
           final bookmark = Bookmark(
             location: location,
+            country: country,
             temperature: temperature,
             highTemp: highTemp,
             lowTemp: lowTemp,
           );
 
+          // Add the new bookmark to the _bookmarks list and save bookmarks
           _bookmarks.add(bookmark);
           _saveBookmarks();
         }
@@ -128,13 +195,33 @@ class _BookmarksPageState extends State<BookmarksPage> {
     });
   }
 
+// Save bookmarks to SharedPreferences
+  Future<void> _saveBookmarks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<Map<String, String>> bookmarks = _bookmarks.map((bookmark) {
+      return {
+        'location': bookmark.location,
+        'country': bookmark.country,
+        'temperature': bookmark.temperature,
+        'highTemp': bookmark.highTemp,
+        'lowTemp': bookmark.lowTemp,
+      };
+    }).toList();
+    final encodedBookmarks = json.encode(bookmarks);
+    prefs.setString('bookmarks', encodedBookmarks);
+  }
+
+// Delete a bookmark at the specified index
   void _deleteBookmark(int index) {
     setState(() {
+      // Remove the bookmark from the _bookmarks list and save bookmarks
       _bookmarks.removeAt(index);
       _saveBookmarks();
     });
   }
 
+
+// Build the bookmarks page UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -174,7 +261,7 @@ class _BookmarksPageState extends State<BookmarksPage> {
                     subtitle: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('country name'),
+                        Text('Country: ${bookmark.country}'), // Use bookmark.country instead of _country
                         Text(
                             'L:${bookmark.lowTemp}°  H:${bookmark.highTemp}°'), // Use bookmark.lowTemp and bookmark.highTemp
                       ],
@@ -276,139 +363,5 @@ class _BookmarksPageState extends State<BookmarksPage> {
         child: const Icon(Icons.add),
       ),
     );
-  }
-
-  void _toggleTextBox() {
-    setState(() {
-      _isDialogVisible = !_isDialogVisible;
-    });
-  }
-
-  Future<void> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      setState(() {
-        _weatherDescription = "Location services are disabled.";
-      });
-      return;
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        setState(() {
-          _weatherDescription = "Location permissions are denied.";
-        });
-        return;
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      setState(() {
-        _weatherDescription = "Location permissions are permanently denied.";
-      });
-      return;
-    }
-
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
-    _fetchWeather(position.latitude, position.longitude);
-    _fetchHourlyForecast(position.latitude, position.longitude);
-  }
-
-  Future<void> _fetchHourlyForecast(double latitude, double longitude) async {
-    final response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&units=metric&appid=$_apiKeyOpenweather'));
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      final List<dynamic> hourlyForecasts = data['list'];
-
-      setState(() {
-        _hourlyForecast = hourlyForecasts.where((forecast) {
-          final DateTime forecastTime =
-              DateTime.fromMillisecondsSinceEpoch(forecast['dt'] * 1000);
-          return forecastTime
-              .isBefore(DateTime.now().add(const Duration(hours: 24)));
-        }).map((forecast) {
-          final DateTime forecastTime =
-              DateTime.fromMillisecondsSinceEpoch(forecast['dt'] * 1000);
-          return {
-            'time': forecastTime,
-            'temperature': forecast['main']['temp'].toStringAsFixed(0),
-            'weatherIcon': forecast['weather'][0]['icon'],
-          };
-        }).toList();
-      });
-    } else {
-      setState(() {
-        _weatherDescription = "Failed to fetch hourly forecast data.";
-      });
-    }
-  }
-
-  Future<void> _fetchWeather(double latitude, double longitude) async {
-    final response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$_apiKeyOpenweather'));
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      setState(() {
-        _location = data['name'];
-        _temperature =
-            double.parse(data['main']['temp'].toString()).toStringAsFixed(0);
-        _weatherDescription = data['weather'][0]['description'];
-        _highTemp = double.parse(data['main']['temp_max'].toString())
-            .toStringAsFixed(0);
-        _lowTemp = double.parse(data['main']['temp_min'].toString())
-            .toStringAsFixed(0);
-        _modifyBookmark(_location, _temperature, _highTemp,
-            _lowTemp); // Call _modifyBookmark here
-      });
-    } else {
-      setState(() {
-        _weatherDescription = "Failed to fetch weather data.";
-      });
-    }
-  }
-
-  Future<void> _fetchWeatherForSearch(String location) async {
-    try {
-      List<Location> locations = await locationFromAddress(location);
-      if (locations.isNotEmpty) {
-        double latitude = locations[0].latitude;
-        double longitude = locations[0].longitude;
-        _fetchWeather(latitude, longitude);
-        _fetchHourlyForecast(latitude, longitude);
-      }
-    } catch (e) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("Location doesn't exist"),
-            content: const Text("Please enter a valid location."),
-            actions: [
-              Center(
-                child: SizedBox(
-                  width: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Center(child: Text('OK')),
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
-      );
-    }
   }
 }
